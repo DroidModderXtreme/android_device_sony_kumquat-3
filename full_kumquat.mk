f5359551f06aafbf0e05da64a3e08c9a5a191f64
Rocco Siffredi <leonardobianco44@gmail.com>
2014-01-19 10:43:59 -0500

    Update full_kumquat.mk

--- full_kumquat.mk
+++ full_kumquat.mk
@@ -36,7 +36,7 @@ $(call inherit-product, device/sony/kumquat/prebuilt/resources-480x854.mk)
 $(call inherit-product, device/sony/kumquat/kumquat.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_kumquat
+PRODUCT_NAME := pac_kumquat
 PRODUCT_DEVICE := kumquat
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
